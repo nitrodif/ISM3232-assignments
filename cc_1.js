313454cb765153ec3f1978858b1ec1a5d0414ead
nitrodif <alex.c22513@gmail.com>
2024-05-17 16:44:13 -0400

    Add output functionality to display bill, tip, and total.

--- cc_1.js
+++ cc_1.js
@@ -1,1 +1,3 @@
 let tips = bills.map(bill => calcTip(bill));
+for (let i = 0; i < bills.length; i++) {
+    console.log(`The bill was \$${bills[i]}, the tip was \$${tips[i]}, and the total value \$${total[i]}`);
