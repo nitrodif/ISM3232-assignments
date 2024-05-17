f8c1267efad82a33d0d4991c60c3a833274ab752
nitrodif <alex.c22513@gmail.com>
2024-05-17 16:41:11 -0400

    Create and test calcTip function.

--- cc_1.js
+++ cc_1.js
@@ -1,3 +1,4 @@
 let tips = bills.map(bill => calcTip(bill));
 for (let i = 0; i < bills.length; i++) {
     console.log(`The bill was \$${[i]}, the tip was \$${[i]}, and the total value \$${[i]}`);
+console.log(calcTip(100)); // 15
