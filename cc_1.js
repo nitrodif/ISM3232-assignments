4ab6637472b4f7b3ecbdad6fe23e6e499d812d1f
nitrodif <alex.c22513@gmail.com>
2024-05-17 16:45:38 -0400

    Delete cc_1.js

--- cc_1.js
+++
@@ -1,5 +0,0 @@
-let tips = bills.map(bill => calcTip(bill));
-for (let i = 0; i < bills.length; i++) {
-    console.log(`The bill was \$${bills[i]}, the tip was \$${tips[i]}, and the total value \$${total[i]}`);
-function calcTip(bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
