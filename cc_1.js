2f5bae06b01498226a25bdfd2bcccbfe161545d9
nitrodif <alex.c22513@gmail.com>
2024-05-17 16:42:58 -0400

    Update cc_1.js

--- cc_1.js
+++ cc_1.js
@@ -1,4 +1,1 @@
-let tips = bills.map(bill => calcTip(bill));
-for (let i = 0; i < bills.length; i++) {
-    console.log(`The bill was \$${[i]}, the tip was \$${[i]}, and the total value \$${[i]}`);
-console.log(calcTip(100)); // 15
+
