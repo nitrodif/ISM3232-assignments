3707c7ac73ba7fc0dfbde6fa85825957a0904b8b
nitrodif <alex.c22513@gmail.com>
2024-05-22 13:31:56 -0400

    Create and test calcTip function.

--- cc_1.js
+++ cc_1.js
@@ -1,2 +1,5 @@
 let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 console.log(`The bill was \$${bill}, the tip was \$${tip}, and the total value \$${bill + tip}`);
+function calcTip(bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  console.log(calcTip(100)); // should print 15
