9cc2d7e968198b7016462e70c3da1de40ac4fc60
nitrodif <alex.c22513@gmail.com>
2024-05-22 13:32:30 -0400

    Implement and populate arrays for bills, tips, and totals.

--- cc_1.js
+++ cc_1.js
@@ -3,3 +3,6 @@ console.log(`The bill was \$${bill}, the tip was \$${tip}, and the total value \
 function calcTip(bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
   console.log(calcTip(100)); // should print 15
+let bills = [275, 40, 430, 125, 555, 44];
+  let tips = bills.map(bill => calcTip(bill));
+  let total = bills.map((bill, i) => bill + tips[i]);
