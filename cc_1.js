d91ff15b619f85ba923c3efb07cbe72c6b280b5b
nitrodif <alex.c22513@gmail.com>
2024-05-24 19:28:16 -0400

    U number

--- cc_1.js
+++ cc_1.js
@@ -1,3 +1,4 @@
+  U74589834
 let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 console.log(`The bill was \$${bill}, the tip was \$${tip}, and the total value \$${bill + tip}`);
 function calcTip(bill) {
